2e8ebb1b0eeaca7f3237c6b647b9ad2db60c23c2
DESKTOP-601IU2J\Scundanomine <scundanomine21@gmail.com>
2023-08-18 23:55:25 +0530

    wow

---
+++ AngelOneSmartAPIApp/GetLoginCredential.py
@@ -0,0 +1,29 @@
+import json
+
+login_credential = ""
+
+
+def get_login_credentials():
+    global login_credential
+
+    def login_credentials():
+        global login_credential
+        print("---- Enter you Zerodha login_credentials ---")
+        login_credential = {"api_key": str(input("Enter API Key :")),
+                            "api_secret": str(input("Enter API Secret :"))
+                            }
+        if input("Press Y to save login_credential and any key to bypass : ").upper() == "Y":
+            with open(f"login_credentials.txt", "w") as f:
+                json.dump(login_credential, f)
+            print("Data Saved. ..")
+        else:
+            print("Data save canceled!!!")
+
+    while True:
+        try:
+            with open(f"login_credentials.txt", "r") as f:
+                login_credential = json.load(f)
+            break
+        except:
+            login_credentials()
+    return login_credential
---
+++ AngelOneSmartAPIApp/test.py
@@ -0,0 +1,7 @@
+from GetLoginCredential import *
+
+
+a = get_login_credentials()
+
+print(a["api_key"])
+print(login_credential)
