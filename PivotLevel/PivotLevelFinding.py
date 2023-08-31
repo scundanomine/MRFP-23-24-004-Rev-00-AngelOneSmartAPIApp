9952c74acf9c0efc6648cf02d2e4142b07ddfaab
DESKTOP-601IU2J\Scundanomine <scundanomine21@gmail.com>
2023-09-01 00:09:15 +0530

    wow

--- PivotLevel/PivotLevelFinding.py
+++ PivotLevel/PivotLevelFinding.py
@@ -2,11 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import xlwings as xw
 
-options = Options()
-options.add_experimental_option("detach", True)
+wb = xw.Book("../AngelOneSmartAPIApp/TA_Python.xlsm")
+dt = wb.sheets("Exchange")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                          options=options)
+varS = pd.DataFrame(dt.range("a1:l202").value)
+varS.columns = varS.iloc[0]
+varS = varS[1:]
 
-driver.get("https://www.google.com/")
+varS["id"] = varS["id"].astype("int64")
+
+varS = varS.drop(columns=['symbol', 'token'])
+
+print(varS["id"])
+
+
+# options = Options()
+# options.add_experimental_option("detach", True)
+#
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+#                           options=options)
+# url = "https://www.topstockresearch.com/PivotPoint/IntradaySupportAndResistanceUsingPivotPoint.html"
+#
+# driver.get(url)
+# driver.maximize_window()
