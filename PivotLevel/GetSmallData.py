9fe5fd6053df2216db26a14f1c177342a1521782
DESKTOP-601IU2J\Scundanomine <scundanomine21@gmail.com>
2023-09-01 21:15:30 +0530

    wow

---
+++ PivotLevel/GetSmallData.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import xlwings as xw
+
+
+def getSmallData():
+    wb = xw.Book("../AngelOneSmartAPIApp/TA_Python.xlsm")
+    dt = wb.sheets("Exchange")
+    varS = pd.DataFrame(dt.range("a1:l202").value)
+    varS.columns = varS.iloc[0]
+    varS = varS[1:]
+    varS["id"] = varS["id"].astype("int64")
+    varS = varS.drop(columns=['symbol', 'token'])
+    return varS
--- PivotLevel/PivotLevelFinding.py
+++ PivotLevel/PivotLevelFinding.py
@@ -2,29 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 import pandas as pd
 import xlwings as xw
+# from GetSmallData import *
 
-wb = xw.Book("../AngelOneSmartAPIApp/TA_Python.xlsm")
-dt = wb.sheets("Exchange")
+# smD = getSmallData()
+options = Options()
+options.add_experimental_option("detach", True)
 
-varS = pd.DataFrame(dt.range("a1:l202").value)
-varS.columns = varS.iloc[0]
-varS = varS[1:]
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                          options=options)
+url = "https://www.topstockresearch.com/PivotPoint/IntradaySupportAndResistanceUsingPivotPoint.html"
 
-varS["id"] = varS["id"].astype("int64")
+driver.get(url)
+driver.maximize_window()
 
-varS = varS.drop(columns=['symbol', 'token'])
+# rows = driver.find_element(By.CSS_SELECTOR, "#results tbody tr")
 
-print(varS["id"])
+rows = driver.find_elements(By.CSS_SELECTOR, "#results tbody tr")
 
-
-# options = Options()
-# options.add_experimental_option("detach", True)
-#
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-#                           options=options)
-# url = "https://www.topstockresearch.com/PivotPoint/IntradaySupportAndResistanceUsingPivotPoint.html"
-#
-# driver.get(url)
-# driver.maximize_window()
+print(rows[1].find_elements(By.CSS_SELECTOR, "td")[1].text)
